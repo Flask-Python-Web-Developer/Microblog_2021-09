8421502db446ccf0efdb4c84d89d0480a584687e
andrewkovalenko1959 <74938298+andrewkovalenko1959@users.noreply.github.com>
2021-06-09 15:18:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template, request
 import datetime
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 # added def create_app():
 # to avoid multiple deployment put next line, app factory
+
+# this video ,120, hiding secrets by addin enviroment variable
+# 1.pip install  python-dotenv
+# 2. create .env file
+#   put any environment variables that we want our code to have access to. in that file
+# 3.add the next : from dotenv import load_dotenv
+# 4. load_dotenv()
+# 5, add .gitignore and put .env - meaning ignore that file
+# 6. add .env.example
+# 7 add: import os,  change client = (os.environ.get("MONGODB_URI"),ssl=True, ssl_cert_reqs='CERT_NONE')
+# 8 add python-dotenv to requirements file
+# 9 add files to github : env.example, gitignore
+#
+# 9 add env var in Heroku
+
+load_dotenv()
 def create_app():
 
 
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://andrey:eUQVbeB5AP6pSUTh@microblog-application.x58ex.mongodb.net/test",
-                         ssl=True, ssl_cert_reqs='CERT_NONE')
+    client = MongoClient(os.environ.get("MONGODB_URI"),ssl=True, ssl_cert_reqs='CERT_NONE')
 
     # client = MongoClient("mongodb+srv://andrey:eUQVbeB5AP6pSUTh@microblog-application.x58ex.mongodb.net/test", ssl=True)
     # https://pymongo.readthedocs.io/en/stable/examples/tls.html
